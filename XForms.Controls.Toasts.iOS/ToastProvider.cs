ffee2b26ea9df7e4a195bfa065f38cd3a83b9973
Alexei Humeniy <ahumeniy@netred.cl>
2017-12-20 12:10:29 -0300

    Implementación iOS

--- XForms.Controls.Toasts.iOS/ToastProvider.cs
+++ XForms.Controls.Toasts.iOS/ToastProvider.cs
@@ -1,11 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
 
-namespace Xamarin.Forms.Controls.Extras.Toast.iOS
+[assembly: Dependency(typeof(Xamarin.Forms.Controls.Extras.iOS.ToastProvider))]
+namespace Xamarin.Forms.Controls.Extras.iOS
 {
-    public class ToastProvider
+    public class ToastProvider : IToastProvider
     {
+        NSTimer alertDelay;
+        ToastView alert;
+
+        public void MakeText(string text)
+        {
+            if (alert != null)
+                DismissMessage();
+            if (alertDelay != null)
+                DismissMessage();
+
+            alertDelay = NSTimer.CreateScheduledTimer(3.5, (obj) => DismissMessage());
+            var bounds = UIScreen.MainScreen.Bounds;
+            alert = new ToastView(bounds, text);
+            UIApplication.SharedApplication.KeyWindow.Add(alert);
+        }
+
+        private void DismissMessage()
+        {
+            if (alert != null)
+                alert.Hide();
+
+            if (alertDelay != null)
+                alertDelay.Dispose();
+
+            alert = null;
+            alertDelay = null;
+        }
     }
 }
---
+++ XForms.Controls.Toasts.iOS/ToastView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+
+using CoreGraphics;
+using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace Xamarin.Forms.Controls.Extras.iOS
+{
+    [Register("ToastView")]
+    public class ToastView : UIView
+    {
+        UILabel textLabel;
+
+        public ToastView()
+        {
+            Initialize();
+        }
+
+        public ToastView(CGRect frame, string text) : base(frame)
+        {
+            BackgroundColor = UIColor.Clear;
+            AutoresizingMask = UIViewAutoresizing.All;
+
+            nfloat labelHeight = 32;
+            nfloat labelWidth = frame.Width - 20;
+
+            nfloat centerX = Frame.Width / 2;
+            nfloat centerY = frame.Height - 20;
+
+            nfloat x = 10;
+            nfloat y = Frame.Height - labelHeight - 20;
+
+            var posrect = new CGRect(x, y, labelWidth, labelHeight);
+
+            textLabel = new UILabel(posrect);
+            textLabel.BackgroundColor = Color.FromHex("#99000000").ToUIColor();
+            textLabel.TextAlignment = UITextAlignment.Center;
+            textLabel.Font = textLabel.Font.WithSize(12);
+            textLabel.AutoresizingMask = UIViewAutoresizing.FlexibleBottomMargin;
+            textLabel.Text = text;
+            textLabel.TextColor = Color.White.ToUIColor();
+            textLabel.Layer.MasksToBounds = true;
+            textLabel.Layer.CornerRadius = 10;
+            UserInteractionEnabled = false;
+
+            AddSubview(textLabel);
+        }
+
+        public void Hide()
+        {
+            UIView.Animate(0.5, () => { Alpha = 0; }, () => { RemoveFromSuperview(); });
+        }
+
+        void Initialize()
+        {
+        }
+    }
+}
